fab4df27ec347984d0200dd36b06442fba02e19c
Bojan Jakimovski <bokicasheks@yahoo.com>
2024-12-19 09:14:27 +0100

    Adding the __init__.py for the whole package

---
+++ synthgen/__init__.py
@@ -0,0 +1,8 @@
+"""SynthGen package for generating Synthetic Raw, Instruction, and Preference Datasets."""
+
+from .dataset_generator import (
+    RawDatasetGenerator,
+    InstructionDatasetGenerator,
+    PreferenceDatasetGenerator,
+)
+from .data_model import DatasetGeneratorConfig, LLMConfig, DatasetConfig
